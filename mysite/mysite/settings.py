da45da3ff985c3b855df786edc7dad04a40cd1e1
sabiqxs <sabiqxs@gmail.com>
2017-02-26 14:45:47 +0700

    django web dev

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'webapp',
+    # 'webapp',
+    'personal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
